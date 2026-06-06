fbd176255ac6c36991c3a0214a0c0de7da8e33b8
DBinK <DBinKv1@Gmail.com>
2026-06-06 21:21:08 +0800

    feat(generics): 修复 generics1 中 Vec<T> 的类型注解，重写 generics2 以支持任意类型

--- exercises/14_generics/generics1.rs
+++ exercises/14_generics/generics1.rs
@@ -5,7 +5,7 @@
 fn main() {
     // TODO: 通过为动态数组 `Vec<T>` 的类型添加注解来修复编译器错误。
     // 选择 `T` 作为某种可以由 `u8` 和 `i8` 创建的整数类型。
-    let mut numbers = Vec::new();
+    let mut numbers: Vec<i32> = Vec::new();
 
     // 不要修改此行代码。
     let n1: u8 = 42;
@@ -15,3 +15,9 @@ fn main() {
 
     println!("{numbers:?}");
 }
+
+// 核心原理：
+// T.into() 会调用 From<T> trait 完成类型转换。
+// i16 同时实现了 From<u8> 和 From<i8>，所以两种类型的值都能通过 .into() 转成 i16。
+// 而 u8 和 i8 之间不能互相转换（i8 可以表示负数，u8 不能；
+// u8 可以表示 128+，i8 不能），所以不能直接用 Vec<u8> 或 Vec<i8>。
